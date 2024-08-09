d1b6998465cf0bf3ec83ddd81e3cb62acdae9129
Samuel Wang <chieh0919@gmail.com>
2024-08-09 07:33:35 +0000

    set rollup.config.js and corresponding settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,20 @@
+import { includeIgnoreFile } from '@eslint/compat';
 import pluginJs from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import tseslint from 'typescript-eslint';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, '.gitignore');
+
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  includeIgnoreFile(gitignorePath),
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+  },
   { files: ['**/*.js'], languageOptions: { sourceType: 'module' } },
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,35 @@
-// rollup.config.js
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'rollup';
+import { dts } from 'rollup-plugin-dts';
+import esbuild from 'rollup-plugin-esbuild';
+import license from 'rollup-plugin-license';
 
-export default defineConfig({
-  /* your config */
+const licensePlugin = license({
+  banner: {
+    commentStyle: 'ignored', // The default
+    content: {
+      file: path.join(path.dirname(fileURLToPath(import.meta.url)), 'LICENSE'),
+    },
+  },
 });
+
+export default defineConfig([
+  {
+    input: 'src/sorting/index.ts',
+    output: {
+      file: 'dist/sorting/index.js',
+      format: 'es',
+      sourcemap: true,
+    },
+    plugins: [esbuild({ minify: true }), licensePlugin],
+  },
+  {
+    input: 'src/sorting/index.ts',
+    output: {
+      file: 'dist/sorting/index.d.ts',
+      format: 'es',
+    },
+    plugins: [dts(), licensePlugin],
+  },
+]);
