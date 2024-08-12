e8864777919f42589ced2eb0e6100c3151460e45
Samuel Wang <chieh0919@gmail.com>
2024-08-12 08:41:26 +0000

    implement getRandomNum() utility function

---
+++ src/utilities/number/getRandomNum.ts
@@ -0,0 +1,12 @@
+/**
+ * A function for getting a random number.
+ * @param min The generated number will be greater than or equal to this minimum number.
+ * @param max The generated number will be less than this maximum number.
+ * @returns The generated number.
+ */
+export function getRandomNum(
+  min: number = 0,
+  max: number = Number.MAX_VALUE
+): number {
+  return Math.random() * max + min;
+}
