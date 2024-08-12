efcb774512e2dbe19315d81dba21956fc0637ab3
Samuel Wang <chieh0919@gmail.com>
2024-08-12 08:34:46 +0000

    add min param and docs comments to getRandomInt() function and update tests

--- src/utilities/number/__tests__/getRandomInt.spec.ts
+++ src/utilities/number/__tests__/getRandomInt.spec.ts
@@ -1,10 +1,35 @@
 import { getRandomInt } from '../getRandomInt';
 
 describe('getRandomInt', () => {
-  it.each([10, 326.78])(
-    'should get a integrer that is less than %d',
+  it.each([10, 123.456])(
+    'should get a integer that is greater than or equal to %d and is less than the maximum of Number',
+    (min: number) => {
+      const result = getRandomInt(min);
+      expect(result >= min).toBeTruthy();
+      expect(result < Number.MAX_VALUE).toBeTruthy();
+      expect(Number.isInteger(result)).toBeTruthy();
+    }
+  );
+
+  it.each([10, 123.456])(
+    'should get a integer that is less than %d',
     (max: number) => {
-      const result = getRandomInt(max);
+      const result = getRandomInt(undefined, max);
+      expect(result >= 0).toBeTruthy();
+      expect(result < max).toBeTruthy();
+      expect(Number.isInteger(result)).toBeTruthy();
+    }
+  );
+
+  it.each([
+    [0, 10],
+    [3.14, 123.456],
+    [10, 100],
+  ])(
+    'should get a integer that is greater than or equal to %d and is less than %d',
+    (min: number, max: number) => {
+      const result = getRandomInt(min, max);
+      expect(result >= min).toBeTruthy();
       expect(result < max).toBeTruthy();
       expect(Number.isInteger(result)).toBeTruthy();
     }
--- src/utilities/number/getRandomInt.ts
+++ src/utilities/number/getRandomInt.ts
@@ -1,3 +1,12 @@
-export function getRandomInt(max: number): number {
-  return Math.floor(Math.random() * max);
+/**
+ * A function for getting a random integer.
+ * @param min The generated integer will be greater than or equal to this minimum number.
+ * @param max The generated integer will be less than this maximum number.
+ * @returns The generated integer.
+ */
+export function getRandomInt(
+  min: number = 0,
+  max: number = Number.MAX_VALUE
+): number {
+  return Math.floor(Math.random() * max) + Math.floor(min);
 }
