dc96bd341c1b03ae95cf5a6132b3add9dd750be6
Samuel Wang <chieh0919@gmail.com>
2024-08-21 07:57:07 +0000

    add docs comments for the node class of the linked list

--- src/linked-list/models/node.model.ts
+++ src/linked-list/models/node.model.ts
@@ -1,6 +1,17 @@
+/**
+ * A node of the linked list.
+ * @typeParam T - Type of the data.
+ */
 export class Node<T> {
-  data: T;
-  next: Node<T> | null;
+  /**
+   * The data of a node.
+   */
+  public data: T;
+
+  /**
+   * The reference of the next node.
+   */
+  public next: Node<T> | null;
 
   constructor(data: T) {
     this.data = data;
